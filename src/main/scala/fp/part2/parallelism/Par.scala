21ab976b61ed484f9ce8a6d7ae9f5eee361d798f
wiem.zin <wiemzin#gmail.com>
2019-01-27 23:25:01 +0100

    mapping law

--- src/main/scala/fp/part2/parallelism/Par.scala
+++ src/main/scala/fp/part2/parallelism/Par.scala
@@ -1,4 +1,4 @@
-package part2.parallelism
+package fp.part2.parallelism
 
 import java.util.concurrent._
 import language.implicitConversions
@@ -52,7 +52,7 @@ object Par {
     */
   private case class UnitFuture[A](get: A) extends Future[A] {
     def cancel(x: Boolean): Boolean = false
-    def get(v: Long,unit: java.util.concurrent.TimeUnit): A = get
+    def get(v: Long, unit: java.util.concurrent.TimeUnit): A = get
     def isCancelled(): Boolean = false
     def isDone(): Boolean = true
   }
@@ -62,24 +62,28 @@ object Par {
     * So we need to run a and b in separate logical thread in order to provide parallel computations!
     * But let's implement map2 assuming that a & b are in separate threads
     */
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
-    val af = run(es)(a)
-    val bf = run(es)(b)
-    UnitFuture(f(af.get, bf.get ))//this impl doesn't respect timeout
-  }
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = run(es)(a)
+      val bf = run(es)(b)
+      UnitFuture(f(af.get, bf.get)) //this impl doesn't respect timeout
+    }
 
-  def map2_timeout[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
-    val af = run(es)(a)
-    val bf = run(es)(b)
-    Map2Future(af, bf, f)
-  }
+  def map2_timeout[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = run(es)(a)
+      val bf = run(es)(b)
+      Map2Future(af, bf, f)
+    }
 
-  case class Map2Future[A, B, C](a: Future[A], b: Future[B], f: (A, B) => C) extends Future[C] {
+  case class Map2Future[A, B, C](a: Future[A], b: Future[B], f: (A, B) => C)
+    extends Future[C] {
     var cache: Option[C] = None
 
     def cancel(x: Boolean): Boolean = a.cancel(x) || b.cancel(x)
     def get: C = compute(Long.MaxValue)
-    def get(v: Long, unit: java.util.concurrent.TimeUnit): C = compute(TimeUnit.NANOSECONDS.convert(v, unit))
+    def get(v: Long, unit: java.util.concurrent.TimeUnit): C =
+      compute(TimeUnit.NANOSECONDS.convert(v, unit))
     def isCancelled(): Boolean = a.isCancelled || b.isCancelled
     def isDone(): Boolean = cache.isDefined
 
@@ -103,9 +107,10 @@ object Par {
     * Marks the computation for concurrent evaluation
     */
   def fork[A](a: => Par[A]): Par[A] =
-    es => es.submit(new Callable[A] {
-      def call: A = run(es)(a).get
-    })
+    es =>
+      es.submit(new Callable[A] {
+        def call: A = run(es)(a).get
+      })
 
   /**
     * wraps the expression `a` for concurrent evaluation
@@ -125,16 +130,19 @@ object Par {
   implicit def toParOps[A](a: Par[A]): ParOps[A] = new ParOps[A](a)
 
   class ParOps[A](a: Par[A]) {
+    def run(implicit es: ExecutorService): Future[A] = Par.run(es)(a)
     def map2[B, C](b: Par[B])(f: (A, B) => C): Par[C] = Par.map2(a, b)(f)
   }
 
-  def map[A, B](a: Par[A])(f: A => B): Par[B] = map2(a, unit(()))((a, _) => f(a))
+  def map[A, B](a: Par[A])(f: A => B): Par[B] =
+    map2(a, unit(()))((a, _) => f(a))
 
   def sortPar(l: Par[List[Int]]): Par[List[Int]] = map(l)(_.sorted)
 
   def sequence[A](ps: List[Par[A]]): Par[List[A]] =
-    ps.foldRight(unit(List.empty[A])) { case ( p, acc) =>
-      map2(p, acc)(_:: _)
+    ps.foldRight(unit(List.empty[A])) {
+      case (p, acc) =>
+        map2(p, acc)(_ :: _)
     }
 
   def parMap[A, B](l: List[A])(f: A => B): Par[List[B]] = {
@@ -146,58 +154,92 @@ object Par {
     parMap(as.filter(f))(identity)
 
   def flatMap[A, B](a: Par[A])(f: A => Par[B]): Par[B] = { s =>
-      val a_val = run(s)(a).get
-      f(a_val)(s)
+    val a_val = run(s)(a).get
+    f(a_val)(s)
   }
 
   def flatten[A](p: Par[Par[A]]): Par[A] = flatMap(p)(identity)
 
-  def map3[A, B, C, D](pa: Par[A], pb: Par[B], pc: Par[C])(f3: (A, B, C) => D): Par[D] =
-    map2(map2(pa, pb){case (a, b) => (a, b)}, pc){ case ((a, b), c) => f3(a, b, c) }
+  def map3[A, B, C, D](pa: Par[A], pb: Par[B], pc: Par[C])(
+    f3: (A, B, C) => D): Par[D] =
+    map2(map2(pa, pb) { case (a, b) => (a, b) }, pc) {
+      case ((a, b), c) => f3(a, b, c)
+    }
 
-  def map4[A, B, C, D, E](pa: Par[A], pb: Par[B], pc: Par[C], pd: Par[D])(f4: (A, B, C, D) => E): Par[E] =
+  def map4[A, B, C, D, E](pa: Par[A], pb: Par[B], pc: Par[C], pd: Par[D])(
+    f4: (A, B, C, D) => E): Par[E] =
     map2(
-      map2(pa, pb){case (a, b) => (a, b)},
-      map2(pc, pd){ case (c, d) => (c, d) }
-    ){
+      map2(pa, pb) { case (a, b) => (a, b) },
+      map2(pc, pd) { case (c, d) => (c, d) }
+    ) {
       case ((a, b), (c, d)) => f4(a, b, c, d)
     }
 
-
-  def map5[A, B, C, D, E, F](pa: Par[A], pb: Par[B], pc: Par[C], pd: Par[D], pe: Par[E])(f5: (A, B, C, D, E) => F): Par[F] =
+  def map5[A, B, C, D, E, F](pa: Par[A],
+                             pb: Par[B],
+                             pc: Par[C],
+                             pd: Par[D],
+                             pe: Par[E])(f5: (A, B, C, D, E) => F): Par[F] =
     map2(
       map2(
-      map2(pa, pb){case (a, b) => (a, b)},
-      map2(pc, pd){ case (c, d) => (c, d) }
-      ){
-      case ((a, b), (c, d)) => (a, b, c, d)
-      }, pe
+        map2(pa, pb) { case (a, b) => (a, b) },
+        map2(pc, pd) { case (c, d) => (c, d) }
+      ) {
+        case ((a, b), (c, d)) => (a, b, c, d)
+      },
+      pe
     ) {
-      case ((a, b, c, d), e) => f5(a, b, c, d, e)d
+      case ((a, b, c, d), e) => f5(a, b, c, d, e)
     }
 
   /**
     * The law of mapping :
-    *     * identity: two expressions are identical or equivalent => map(unit(1))(_ + 1) is equivalent to unit(2).
-    *     In order to check that let's implemen a function: equals
+    *     * identity: two expressions are identical or equivalent: id(a) = a
+    *     map(unit(1))(_ + 1) is equivalent to unit(2).
+    *     * composition: map(map(y)(g))(f) == map (y) ( f compose g )
+    *     How to prove it?
+    *     Given
+    *     f compose g = p compose q
+    *     map f compose map g = map p compose map q
+    *     Assuming that p = id and q = f compose g
+    *     map f compose map g
+    *   = map id compose map (f compose g) //id(a) = a
+    *   = map (f compose g)
+    *   So, map f compose map g = map (f compose g)
+    *   See more: https://github.com/quchen/articles/blob/master/second_functor_law.md
     */
-
-  def equals[A](e: ExecutorService)(p1: Par[A], p2: Par[B]): Boolean = p1(e).get == p2(e).get
+  /**
+    * To check the identity law
+    */
+  def equals[A, B](e: ExecutorService)(p1: Par[A], p2: Par[B]): Boolean =
+    p1(e).get == p2(e).get
+  /**
+    * The law of foking:
+    * fork(x) should do the same thing as x  but asynchronously:
+    * fork(x) == x
+    *
+    */
 
 }
 
-object example {
+object example extends App {
   import Par._
 
   def sum(ints: IndexedSeq[Int]): Par[Int] =
-    if (ints.size <= 1) Par.unit(ints.headOption getOrElse(0))
+    if (ints.size <= 1) Par.unit(ints.headOption getOrElse (0))
     else {
       val (l, r) = ints.splitAt(ints.size / 2)
       Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
     }
 
-  def max(ints: IndexedSeq[Int]): Par[Int] = Par.map(Par.sortPar(unit(ints.toList)))(_.last)
+  def max(ints: IndexedSeq[Int]): Par[Int] =
+    Par.map(Par.sortPar(unit(ints.toList)))(_.last)
 
   def getNbWords(paragraphs: List[String]): Par[Int] =
-    Par.map(unit(paragraphs))(p => p.flatMap(_.split(" ")).size)
+    Par.map(unit(paragraphs))(_.flatMap(_.split(" ")).size)
+
+  //  val es = Executors.newFixedThreadPool(8)
+  //  val s = sum(1 to 30).run(es).get
+  //
+  //  println(s"the sum of 1 .. 100 is: $s")
 }
